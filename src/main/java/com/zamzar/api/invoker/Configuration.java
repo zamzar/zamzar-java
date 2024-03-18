098ccfd1c161e2c5165d6f285eefc9ca800a15b2
louismrose <louismrose@users.noreply.github.com>
2024-03-18 13:19:53 +0000

    Bump version to v0.0.2

--- src/main/java/com/zamzar/api/invoker/Configuration.java
+++ src/main/java/com/zamzar/api/invoker/Configuration.java
@@ -15,7 +15,7 @@ package com.zamzar.api.invoker;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.2";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
