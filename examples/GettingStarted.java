83a98d8a14100069379272cbedfb77213080df52
Louis Rose <mail@louismrose.com>
2024-03-18 14:17:41 +0000

    updates README

---
+++ examples/GettingStarted.java
@@ -0,0 +1,18 @@
+import com.zamzar.api.ZamzarClient;
+import com.zamzar.api.invoker.ApiException;
+
+import java.io.File;
+
+public class GettingStarted {
+    public static void main(String[] args) throws ApiException {
+        // Signup for a Zamzar API Account or retrieve your existing API Key from https://developers.zamzar.com
+        ZamzarClient client = new ZamzarClient("YOUR_API_KEY_GOES_HERE");
+
+        // Converts /tmp/example.docx to /tmp/example.pdf
+        client
+            .convert(new File("/tmp/example.docx"), "pdf") // uploads and converts your file
+            .awaitOrThrow() // waits for completion, throwing an exception on failure
+            .store(new File("/tmp/example.zip")) // downloads the converted file
+            .deleteAllFiles(); // removes your files (example.docx and example.pdf) from Zamzar's servers
+    }
+}
