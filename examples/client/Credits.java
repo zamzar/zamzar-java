7f952c1e69ee60427787e2542a7d67ace608d71d
Louis Rose <mail@louismrose.com>
2024-03-19 10:31:30 +0000

    improves examples

--- examples/client/Credits.java
+++ examples/client/Credits.java
@@ -1,17 +1,20 @@
 package client;
 
 import com.zamzar.api.ZamzarClient;
+import com.zamzar.api.invoker.ApiException;
 
 public class Credits {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ApiException {
         ZamzarClient zamzar = new ZamzarClient("YOUR_API_KEY_GOES_HERE");
 
         // Make an API call to get the remaining credits
-        int remainingCredits = zamzar.getLastProductionCreditsRemaining();
-        int remainingTestCredits = zamzar.getLastSandboxCreditsRemaining();
+        int remainingCredits = zamzar.getProductionCreditsRemaining();
+        int remainingTestCredits = zamzar.getSandboxCreditsRemaining();
+        System.out.println("You currently have " + remainingCredits + " production credits remaining and " + remainingTestCredits + " test credits remaining.");
 
         // Or, if you've made an API call already (such as to convert a file), retrieve the last known credits
         remainingCredits = zamzar.getLastProductionCreditsRemaining();
         remainingTestCredits = zamzar.getLastSandboxCreditsRemaining();
+        System.out.println("At the time of your last request, you had " + remainingCredits + " production credits remaining and " + remainingTestCredits + " test credits remaining.");
     }
 }
--- examples/formats/RetrieveFormats.java
+++ examples/formats/RetrieveFormats.java
@@ -6,29 +6,26 @@ import com.zamzar.api.model.Format;
 import com.zamzar.api.pagination.Anchor;
 import com.zamzar.api.pagination.Paged;
 
-import java.util.List;
-
 public class RetrieveFormats {
     public static void main(String[] args) throws ApiException {
         ZamzarClient zamzar = new ZamzarClient("YOUR_API_KEY_GOES_HERE");
 
         // Retrieve information about a single format
-        Format jpg = zamzar.formats().find("jpg");
-        System.out.println(jpg);
-
-        // Viewing the target conversion options for a format
-        jpg
+        System.out.println("jpgs can be converted to:");
+        zamzar.formats().find("jpg")
             .getTargets()
-            .forEach(t -> System.out.println(t.getName() + " (" + t.getCreditCost() + " credits)"));
+            .forEach(t -> System.out.println(" - " + t.getName() + " (" + t.getCreditCost() + " credits)"));
 
         // List all formats
-        List<Format> formats = zamzar.formats().list().getItems();
+        for (Format format : zamzar.formats().list().getItems()) {
+            System.out.println(format.getName());
+        }
 
         // List will return at most 50 formats at a time; to paginate:
         Paged<Format, String> currentPage = zamzar.formats().list();
         do {
             for (Format format : currentPage.getItems()) {
-                System.out.println(format);
+                System.out.println(format.getName());
             }
             currentPage = currentPage.nextPage();
         } while (!currentPage.getItems().isEmpty());
