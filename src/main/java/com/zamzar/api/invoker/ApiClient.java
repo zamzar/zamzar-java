9afa408050198ee5abb9fa97d052593121f9cfda
louismrose <77096+louismrose@users.noreply.github.com>
2026-02-12 17:11:11 +0000

    Bump version to v1.1.7

--- src/main/java/com/zamzar/api/invoker/ApiClient.java
+++ src/main/java/com/zamzar/api/invoker/ApiClient.java
@@ -146,7 +146,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/1.1.6/java");
+        setUserAgent("OpenAPI-Generator/1.1.7/java");
 
         authentications = new HashMap<String, Authentication>();
     }
