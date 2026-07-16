5c4df553511cda40f37b5df3d73ce077fae13c9c
louismrose <77096+louismrose@users.noreply.github.com>
2026-07-16 08:37:58 +0000

    Bump version to v1.1.11

--- src/main/java/com/zamzar/api/invoker/Configuration.java
+++ src/main/java/com/zamzar/api/invoker/Configuration.java
@@ -19,7 +19,7 @@ import java.util.function.Supplier;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.23.0")
 public class Configuration {
-  public static final String VERSION = "1.1.10";
+  public static final String VERSION = "1.1.11";
 
   private static final AtomicReference<ApiClient> defaultApiClient = new AtomicReference<>();
   private static volatile Supplier<ApiClient> apiClientFactory = ApiClient::new;
