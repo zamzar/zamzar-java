11dc474cd012e60793580932544f7007b30cf0e6
louismrose <77096+louismrose@users.noreply.github.com>
2026-09-07 10:30:20 +0000

    Bump version to v1.1.13

--- src/main/java/com/zamzar/api/invoker/ApiClient.java
+++ src/main/java/com/zamzar/api/invoker/ApiClient.java
@@ -147,7 +147,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/1.1.12/java");
+        setUserAgent("OpenAPI-Generator/1.1.13/java");
 
         authentications = new HashMap<String, Authentication>();
     }
--- src/main/java/com/zamzar/api/invoker/Configuration.java
+++ src/main/java/com/zamzar/api/invoker/Configuration.java
@@ -19,7 +19,7 @@ import java.util.function.Supplier;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.25.0")
 public class Configuration {
-  public static final String VERSION = "1.1.12";
+  public static final String VERSION = "1.1.13";
 
   private static final AtomicReference<ApiClient> defaultApiClient = new AtomicReference<>();
   private static volatile Supplier<ApiClient> apiClientFactory = ApiClient::new;
